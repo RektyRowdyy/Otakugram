1a704b6a736de7880d13a1496803a3e30bb5af35
RektyRowdyy <rektyrowdy@gmail.com>
2024-07-03 15:34:58 +0530

    created a constants file for all the static data

--- Otakugram-UI/frontend/src/components/Home/LatestAnimeSlider/LatestAnimeSlider.jsx
+++ Otakugram-UI/frontend/src/components/Home/LatestAnimeSlider/LatestAnimeSlider.jsx
@@ -1,35 +1,10 @@
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import { BsDashLg } from "react-icons/bs";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./LatestAnimeSlider.css"
-
-
-// Function to Create Custom Arrows
-function CustomArrows(props) {
-
-    const { className, style, onClick, arrowType } = props;
-
-    if (arrowType === 'next') {
-        return (
-            <FaChevronRight
-                className={className}
-                style={{ ...style, color: "white", right: "1%", zIndex: 1, height: "30px" }}
-                onClick={onClick}
-            />
-        );
-    }
-    else if (arrowType === 'prev') {
-        return (
-            <FaChevronLeft
-                className={className}
-                style={{ ...style, color: "white", left: "1%", zIndex: 1, height: "30px" }}
-                onClick={onClick}
-            />
-        );
-    }
-}
+import { CustomArrows } from "../../../constants.jsx";
+import { latestAnimeSlides } from "../../../constants.jsx";
 
 // Creating Custom Dot
 const CustomDot = ({ onClick }) => {
@@ -40,14 +15,6 @@ const CustomDot = ({ onClick }) => {
 }
 
 export default function LatestAnimeSlider() {
-
-    const slides = [
-        { url: "src/assets/img/hero-background-1.jpeg", title: 'One Piece' },
-        { url: "src/assets/img/hero-background-2.jpg", title: 'Haikyuu' },
-        { url: "src/assets/img/hero-background-3.jpg", title: 'Naruto' },
-        { url: "src/assets/img/hero-background-4.png", title: 'Kaguya Sama' },
-        { url: "src/assets/img/hero-background-5.jpg", title: 'AOT' },
-    ]
 
     //Slider Settings
     const settings = {
@@ -72,7 +39,7 @@ export default function LatestAnimeSlider() {
             <div>
                 <div>
                     <Slider className="hero" {...settings} >
-                        {slides.map((slide, index) => (
+                        {latestAnimeSlides.map((slide, index) => (
                             <img key={slide.title} src={slide.url} alt="hero-background" className="hero-image" />
                         ))}
                     </Slider>
